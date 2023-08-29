7ba6344b490d0e281195bd40f97c2a5056b018e1
JWL <jwl.vdonsk@gmail.com>
2023-08-29 15:33:44 +0300

    Episode 27 - Category Filters

--- app/Http/Livewire/StatusFilter.php
+++ app/Http/Livewire/StatusFilter.php
@@ -9,20 +9,16 @@ use Livewire\Component;
 
 class StatusFilter extends Component
 {
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-        'status',
-    ];
 
     public function mount()
     {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
@@ -34,12 +30,13 @@ class StatusFilter extends Component
     public function setStatus($newStatus)
     {
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
-//        if ($this->getPreviousRouteName() === 'idea.show') {
+        if ($this->getPreviousRouteName() === 'idea.show') {
             return redirect()->route('idea.index', [
                 'status' => $this->status,
             ]);
-//        }
+        }
     }
 
     public function render()
