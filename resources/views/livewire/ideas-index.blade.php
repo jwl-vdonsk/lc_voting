dd10e45deed28a48a97d8a70dc020833562b3ad7
JWL <jwl.vdonsk@gmail.com>
2023-08-29 13:47:25 +0300

    Episode 25 - Status Filters Count

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -41,6 +41,8 @@
     </div> <!-- end ideas container -->
 
     <div class="my-8">
-        {{ $ideas->links() }}
+{{--        {{ $ideas->links() }}--}}
+        {{ $ideas->appends(request()->query())->links() }}
     </div>
+
 </div>
--- resources/views/livewire/status-filter.blade.php
+++ resources/views/livewire/status-filter.blade.php
@@ -5,17 +5,17 @@
             <li><a wire:click.prevent="setStatus('All')"
                    href="#"
                    class="transition duration-150 ease-in border-b-4 pb-3 hover:border-blue @if ($status === 'All') border-blue text-gray-900 @endif"
-                >All Ideas (87)</a>
+                >All Ideas ({{ $statusCount['all_statuses'] }})</a>
             </li>
             <li><a wire:click.prevent="setStatus('Considering')"
                    href="#"
                    class="transition duration-150 ease-in border-b-4 pb-3 hover:border-purple @if ($status === 'Considering') border-purple text-gray-900 @endif"
-                >Considering (6)</a>
+                >Considering ({{ $statusCount['considering'] }})</a>
             </li>
             <li><a wire:click.prevent="setStatus('In Progress')"
                    href="#"
                    class="transition duration-150 ease-in border-b-4 pb-3 hover:border-yellow @if ($status === 'In Progress') border-yellow text-gray-900 @endif"
-                >In progress (1)</a>
+                >In progress ({{ $statusCount['in_progress'] }})</a>
             </li>
         </ul>
 
@@ -23,12 +23,12 @@
             <li><a wire:click.prevent="setStatus('Implemented')"
                    href="#"
                    class="transition duration-150 ease-in border-b-4 pb-3 hover:border-green @if ($status === 'Implemented') border-green text-gray-900 @endif"
-                >Implemented (10)</a>
+                >Implemented ({{ $statusCount['implemented'] }})</a>
             </li>
             <li><a wire:click.prevent="setStatus('Closed')"
                    href="#"
                    class="transition duration-150 ease-in border-b-4 pb-3 hover:border-red @if ($status === 'Closed') border-red text-gray-900 @endif"
-                >Closed (55)</a>
+                >Closed ({{ $statusCount['closed'] }})</a>
             </li>
         </ul>
 
