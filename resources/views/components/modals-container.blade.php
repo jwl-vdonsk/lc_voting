3a71580e0a56202978d2ae61a3a34062618ef00e
JWL <jwl.vdonsk@gmail.com>
2023-09-20 15:34:53 +0300

    Episode 45 - Refacroting Modals

---
+++ resources/views/components/modals-container.blade.php
@@ -0,0 +1,15 @@
+@can('update', $idea)
+    <livewire:edit-idea :idea="$idea" />
+@endcan
+
+@auth
+    <livewire:mark-idea-as-spam :idea="$idea" />
+@endauth
+
+@auth
+    <livewire:mark-idea-as-not-spam :idea="$idea" />
+@endauth
+
+@can('delete', $idea)
+    <livewire:delete-idea :idea="$idea" />
+@endcan
